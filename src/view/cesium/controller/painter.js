7f54a2eab253ff083d2db423307b997e2f0bdfac
DESKTOP-9F9FSSP\budblack <budblack@qq.com>
2017-08-31 16:14:59 +0800

    载入模型

--- src/view/cesium/controller/painter.js
+++ src/view/cesium/controller/painter.js
@@ -7,55 +7,134 @@ let {eventEmitter: em} = comm.core,
 module.exports = {
     drawLabelWithDegress({lng, lat, height, text}) {
         em.emit('dev', {msg: '绘制标签'});
-        let viewer = viewer || cesium.viewer;
-
-        if (viewer) {
-            let scene = feela.cesium.inst.viewer.scene,
-                labels = scene.primitives.add(new Cesium.LabelCollection());
-
-            labels.modelMatrix = Cesium
-                .Transforms
-                .eastNorthUpToFixedFrame(
-                    Cesium.Cartesian3.fromDegrees(lng, lat)
-                );
-
-            labels.add(
-                {
-                    position: new Cesium.Cartesian3(0, 0, 0 + height),
-                    text: text
-                }
+        let scene = feela.cesium.inst.viewer.scene,
+            labels = scene.primitives
+                .add(new Cesium.LabelCollection());
+        labels.modelMatrix = Cesium
+            .Transforms
+            .eastNorthUpToFixedFrame(
+                Cesium.Cartesian3.fromDegrees(
+                    lng,
+                    lat,
+                    height
+                )
             );
-        }
+        labels.add(
+            {
+                position: new Cesium.Cartesian3(
+                    (
+                        0, 0, 0
+                    )),
+                text: text
+            });
     },
 
     addModel({lng, lat, height, url}) {
+
+        let {cesium} = comm,
+            {viewer} = cesium,
+            {scene} = viewer;
         let modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
-            Cesium.Cartesian3.fromDegrees(105, 35, 1000));
-        let scene = cesium.viewer.scene;
-        let model = scene
-            .primitive
-            .add(
-                Cesium
-                    .Model
-                    .fromGltf('/assets/model/gltf/obj0828.gltf')
-            );
+            Cesium.Cartesian3.fromDegrees(lng, lat, -1020)
+        );
+        let seta = 3.14 / 3;
+        let rotaleMatrix = Cesium.Matrix4
+                .fromArray(
+                    [
+                        Math.cos(seta), -1 * Math.sin(seta), 0, 0,
+                        Math.sin(seta), Math.cos(seta), 0, 0,
+                        0, 0, 1, 0,
+                        0, 0, 0, 1
+                    ]
+                ),
+            panMatrix = Cesium.Matrix4
+                .fromArray(
+                    [
+                        1, 0, 0, 0,
+                        0, 1, 0, 0,
+                        0, 0, 1, 0,
+                        300, 100, -200, 1
+                    ]
+                ),
+            resultMatrix = [];
+        Cesium.Matrix4.multiply(modelMatrix, rotaleMatrix, resultMatrix);
+        Cesium.Matrix4.multiply(resultMatrix, panMatrix, resultMatrix);
+        console.log({modelMatrix, rotaleMatrix, resultMatrix});
+        // modelMatrix[0] = modelMatrix[0] * Math.cos(seta);
+        // modelMatrix[1] = modelMatrix[1] * Math.sin(seta) * -1;
+        // modelMatrix[4] = modelMatrix[4] * Math.sin(seta);
+        // modelMatrix[5] = modelMatrix[5] * Math.cos(seta);
+
+        var model = scene.primitives.add(Cesium.Model.fromGltf(
+            {
+                url: '/assets/model/gltf/obj0828.gltf',
+                modelMatrix: resultMatrix,
+                scale: 1.0,
+                rotale: 90
+            }
+        ));
+
     },
 
     drawCylinder({
-                     length = 200000,
-                     topRadius = 80000,
-                     bottomRadius = 200000
+                     lng, lat, height, r, z
                  }) {
         let cylinder = new Cesium.CylinderGeometry(
             {
-                length,
-                topRadius,
-                bottomRadius,
+                length: z,
+                topRadius: r,
+                bottomRadius: r,
             }
         );
-        let geometry = Cesium.CylinderGeometry.createGeometry(cylinder);
+        let instance = new Cesium.GeometryInstance({
+            geometry: cylinder,
+            modelMatrix: Cesium.Matrix4
+                .multiplyByTranslation(
+                    Cesium.Transforms
+                        .eastNorthUpToFixedFrame(
+                            Cesium.Cartesian3
+                                .fromDegrees(lng, lat, height)),
+                    new Cesium.Cartesian3(lng, lat, height),
+                    new Cesium.Matrix4()
+                ),
+            id: 'cylinder',
+            attributes: {
+                color: new Cesium.GeometryInstanceAttribute({
+                    componentDatatype: Cesium.ComponentDatatype.UNSIGNED_BYTE,
+                    componentsPerAttribute: 4,
+                    normalize: true,
+                    value: [102, 124, 255, 0.8]
+                })
+            }
+        });
+        this._addPrimitives(instance);
+    },
 
-        this._addPrimitives(geometry);
+    drawBox({lng, lat, height, x, y, z}) {
+        let instance = new Cesium.GeometryInstance({
+            geometry: Cesium.BoxGeometry.fromDimensions({
+                dimensions: new Cesium.Cartesian3(x, y, z)
+            }),
+            modelMatrix: Cesium.Matrix4
+                .multiplyByTranslation(
+                    Cesium.Transforms
+                        .eastNorthUpToFixedFrame(
+                            Cesium.Cartesian3
+                                .fromDegrees(lng, lat)),
+                    new Cesium.Cartesian3(lng, lat, height),
+                    new Cesium.Matrix4()
+                ),
+            id: 'box',
+            attributes: {
+                color: new Cesium.GeometryInstanceAttribute({
+                    componentDatatype: Cesium.ComponentDatatype.UNSIGNED_BYTE,
+                    componentsPerAttribute: 4,
+                    normalize: true,
+                    value: [255, 255, 0, 255]
+                })
+            }
+        });
+        this._addPrimitives(instance);
     },
 
     drawCircle({lng, lat, r}) {
@@ -83,6 +162,26 @@ module.exports = {
             }
         );
         this._addPrimitives(cricleInstance);
+    },
+
+    createModel({}) {
+
+    },
+
+    _createInstance({geometry}) {
+        let instance = new Cesium.GeometryInstance(
+            {
+                geometry,
+                attributes: {
+                    color: Cesium.ColorGeometryInstanceAttribute.fromColor(
+                        new Cesium.Color(0.4, 0.8, 1, 0.8)
+                    )
+                },
+
+            }
+        );
+
+        return instance;
     },
     _addPrimitives(geometry) {
         let {cesium} = comm,
--- src/view/cesium/index.vue
+++ src/view/cesium/index.vue
@@ -1,5 +1,5 @@
 <template>
-        <div id="cesiumContainer" :class="$style.cesiumContainer"></div>
+    <div id="cesiumContainer" :class="$style.cesiumContainer"></div>
 </template>
 <style lang="less" module>
     .cesiumContainer {
@@ -24,130 +24,133 @@
     }
 </style>
 <script>
-  require('Cesium/Source/Widgets/widgets.css');
-  const Cesium = require('Cesium/Source/Cesium'),
-      comm = require('../_comm'),
-      config = require('../../config'),
-      { core, ui } = comm,
-      { eventEmitter: em } = core;
+    require('Cesium/Source/Widgets/widgets.css');
+    const Cesium = require('Cesium/Source/Cesium'),
+        comm = require('../_comm'),
+        config = require('../../config'),
+        {core, ui} = comm,
+        {eventEmitter: em} = core;
 
-  if (window.CESIUM_BASE_URL == undefined) {
-    window.CESIUM_BASE_URL = './Cesium/';
-  }
-  window.Cesium = Cesium;
-
-  let inst = {
-    viewer: null,
-  };
-
-  export default {
-    data(){
-      return {}
-    },
-    methods  : {
-      /**
-       *
-       * @param {{}} viewer Cesium 视图对象
-       */
-      initHandles     : async function (viewer) {
-        /******************************************************************
-         * 屏幕区域事件 Handle
-         ******************************************************************/
-        /**
-         * @type {*} 屏幕事件句柄
-         */
-        const screenSpaceEventHandler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
-        /**
-         * setInputAction. Set a function to be executed on an input event.
-         * But I dont know what the 'INPUT EVENT' means to.
-         *
-         * By the way, I only going to transfer events to app inside event bus without any meaninful process.
-         */
-        screenSpaceEventHandler.setInputAction(function (movement) {
-          let windowPosition       = viewer.camera.getPickRay(movement.position),
-              cartesianCoordinates = viewer.scene.globe.pick(windowPosition, viewer.scene),
-              cartoCoordinates     = {};
-          if (cartesianCoordinates) {
-            cartoCoordinates = Cesium.Cartographic.fromCartesian(cartesianCoordinates)
-
-            let lng    = Cesium.Math.toDegrees(cartoCoordinates.longitude),
-                lat    = Cesium.Math.toDegrees(cartoCoordinates.latitude),
-                height = Cesium.Math.toDegrees(cartoCoordinates.height);
-
-            movement.wgs = { lng, lat, height };
-
-            em.emit('cesium/screen/input', movement);
-          }
-
-        }, Cesium.ScreenSpaceEventType.LEFT_CLICK)
-      },
-      initCesiumModule: async function () {
-        let cesium_viewer_container = 'cesiumContainer',
-            cesium_viewer_option    = {
-              animation         : false, //动画控制，默认true
-              fullscreenButton  : true,//全屏按钮,默认显示true
-              geocoder          : false,//地名查找,默认true
-              timeline          : false,//时间线,默认true
-              vrButton          : true,//双屏模式,默认不显示false
-              homeButton        : true,//主页按钮，默认true
-              infoBox           : false,//点击要素之后显示的信息,默认true
-              selectionIndicator: true,//选中元素显示,默认true
-              baseLayerPicker   : false,
-              imageryProvider   : new Cesium.BingMapsImageryProvider(
-                  {
-                    url     : 'https://dev.virtualearth.net',
-                    key     : 'Ary1KHQ-df1nT8n6-YCSuBuJ0Vb8QphijrDRuNuaKQBnUGlyj52DE3deZATn9w8h',
-                    mapStyle: Cesium.BingMapsStyle.AERIAL
-                  }
-              ),
-              //                imageryProvider   : new Cesium.WebMapServiceImageryProvider(
-              //                    {
-              //                      url    : 'http://www.scgis.net.cn/imap/iMapServer/defaultRest/services/newtianditudom/WMS',
-              //                      layers : '聚合天地图DOM瓦片地图服务',
-              //                      version: '1.3.0'
-              //                    }
-              //                ),
-              //                imageryProvider   : new Cesium.WebMapTileServiceImageryProvider(
-              //                    {
-              //                      url             : 'http://t0.tianditu.com/img_w/wmts?',
-              //                      layer           : "img",
-              //                      style           : 'default',
-              //                      format          : 'titles',
-              //                      titleMatrixSetID: 'w',
-              //                      credit          : new Cesium.Credit('天地图全球影像服务'),
-              //                      maximumLevel    : 18
-              //                    }
-              //                ),
-              terrainProvider   : new Cesium.CesiumTerrainProvider(config.assets.terrain.CesiumTerrain7Niu)
-            };
-
-        let viewer  = new Cesium.Viewer(
-            cesium_viewer_container,
-            cesium_viewer_option
-        );
-        inst.viewer = viewer;
-        comm.cesium = inst;
-
-        viewer._cesiumWidget._creditContainer.style.display = 'none';
-        return viewer;
-      },
-    },
-    mounted  : async function () {
-      try {
-        let viewer = await this.initCesiumModule();
-        await this.initHandles(viewer)
-      }
-      catch (e) {}
-      finally {
-        em.emit('cesium/core/init');
-      }
-    },
-    beforeRouteEnter (to, from, next) {
-      next();
-    },
-    exportAPI: {
-      controller: require('./controller'),
-      inst      : inst
+    if (window.CESIUM_BASE_URL == undefined) {
+        window.CESIUM_BASE_URL = './Cesium/';
     }
-  }
+    window.Cesium = Cesium;
+
+    let inst = {
+        viewer: null,
+    };
+
+    export default {
+        data() {
+            return {}
+        },
+        methods: {
+            /**
+             *
+             * @param {{}} viewer Cesium 视图对象
+             */
+            initHandles: async function (viewer) {
+                /******************************************************************
+                 * 屏幕区域事件 Handle
+                 ******************************************************************/
+                /**
+                 * @type {*} 屏幕事件句柄
+                 */
+                const screenSpaceEventHandler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+                /**
+                 * setInputAction. Set a function to be executed on an input event.
+                 * But I dont know what the 'INPUT EVENT' means to.
+                 *
+                 * By the way, I only going to transfer events to app inside event bus without any meaninful process.
+                 */
+                screenSpaceEventHandler.setInputAction(function (movement) {
+                    let windowPosition = viewer.camera.getPickRay(movement.position),
+                        cartesianCoordinates = viewer.scene.globe.pick(windowPosition, viewer.scene),
+                        cartoCoordinates = {};
+                    if (cartesianCoordinates) {
+                        cartoCoordinates = Cesium.Cartographic.fromCartesian(cartesianCoordinates)
+
+                        let lng = Cesium.Math.toDegrees(cartoCoordinates.longitude),
+                            lat = Cesium.Math.toDegrees(cartoCoordinates.latitude),
+                            height = Cesium.Math.toDegrees(cartoCoordinates.height);
+
+                        movement.wgs = {lng, lat, height};
+
+                        em.emit('cesium/screen/input', movement);
+                    }
+
+                }, Cesium.ScreenSpaceEventType.LEFT_CLICK)
+            },
+            initCesiumModule: async function () {
+                let cesium_viewer_container = 'cesiumContainer',
+                    cesium_viewer_option = {
+                        animation: false, //动画控制，默认true
+                        fullscreenButton: true,//全屏按钮,默认显示true
+                        geocoder: false,//地名查找,默认true
+                        timeline: false,//时间线,默认true
+                        vrButton: true,//双屏模式,默认不显示false
+                        homeButton: true,//主页按钮，默认true
+                        infoBox: false,//点击要素之后显示的信息,默认true
+                        selectionIndicator: true,//选中元素显示,默认true
+                        baseLayerPicker: false,
+                        imageryProvider: new Cesium.BingMapsImageryProvider(
+                            {
+                                url: 'https://dev.virtualearth.net',
+                                key: 'Ary1KHQ-df1nT8n6-YCSuBuJ0Vb8QphijrDRuNuaKQBnUGlyj52DE3deZATn9w8h',
+                                mapStyle: Cesium.BingMapsStyle.AERIAL
+                            }
+                        ),
+                        //                imageryProvider   : new Cesium.WebMapServiceImageryProvider(
+                        //                    {
+                        //                      url    : 'http://www.scgis.net.cn/imap/iMapServer/defaultRest/services/newtianditudom/WMS',
+                        //                      layers : '聚合天地图DOM瓦片地图服务',
+                        //                      version: '1.3.0'
+                        //                    }
+                        //                ),
+                        //                imageryProvider   : new Cesium.WebMapTileServiceImageryProvider(
+                        //                    {
+                        //                      url             : 'http://t0.tianditu.com/img_w/wmts?',
+                        //                      layer           : "img",
+                        //                      style           : 'default',
+                        //                      format          : 'titles',
+                        //                      titleMatrixSetID: 'w',
+                        //                      credit          : new Cesium.Credit('天地图全球影像服务'),
+                        //                      maximumLevel    : 18
+                        //                    }
+                        //                ),
+                        terrainProvider: new Cesium.CesiumTerrainProvider(config.assets.terrain.CesiumTerrain7Niu)
+                    };
+
+                let viewer = new Cesium.Viewer(
+                    cesium_viewer_container,
+                    cesium_viewer_option
+                );
+                viewer.scene.globe.depthTestAgainstTerrain = false;
+
+                inst.viewer = viewer;
+                comm.cesium = inst;
+
+                viewer._cesiumWidget._creditContainer.style.display = 'none';
+                return viewer;
+            },
+        },
+        mounted: async function () {
+            try {
+                let viewer = await this.initCesiumModule();
+                await this.initHandles(viewer)
+            }
+            catch (e) {
+            }
+            finally {
+                em.emit('cesium/core/init');
+            }
+        },
+        beforeRouteEnter(to, from, next) {
+            next();
+        },
+        exportAPI: {
+            controller: require('./controller'),
+            inst: inst
+        }
+    }
 </script>
